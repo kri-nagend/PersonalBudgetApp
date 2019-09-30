144eca20b7c95fc1e5e4ab87f746e7fccba8f71c
kri-nagend <kri.nagend@gmail.com>
2019-09-29 21:02:30 -0400

    added ability to group expenses by type

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 
+mydict = {}
+
 with open('bday.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -9,5 +11,12 @@ with open('bday.csv') as csv_file:
             line_count += 1
         else:
             print(f'\t on {row[0]} a {row[3]} {row[1]} was made at {row[2]} for ${row[4]}')
+            if(row[3] in mydict):
+                mydict[row[3]]+= float(row[4])
+            else:
+                mydict[row[3]] = float(row[4])
+
             line_count += 1
     print(f'Processed {line_count} lines.')
+
+    print(mydict)
