328e08766e78cbebc562b7cc8833890206d18a13
kri-nagend <kri.nagend@gmail.com>
2019-10-02 20:16:33 -0400

    filtered transactions by month

--- main.py
+++ main.py
@@ -4,19 +4,17 @@ import shutil
 import pandas as pd
 import glob
 
-mydict = {}
-
-path = r'./Files/RogersMC/' # use your path
+path = r'./Files/RogersMC/'
 frame = pd.concat([pd.read_csv(f, sep=',') for f in glob.glob(path + "/*.csv")], ignore_index=True)
 
 frame.to_csv( "./Files/RogersMC/res/combined_csv.csv", index=False, encoding='utf-8-sig')
 
-
-mydict = {}
 with open('./Files/RogersMC/res/combined_csv.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     mydict = {}
+    sept = {}
+    x = 0
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
@@ -28,10 +26,26 @@ with open('./Files/RogersMC/res/combined_csv.csv') as csv_file:
             else:
                 mydict[row[3]] = float(row[4])
 
+            if row[0][6] == '9':
+                if row[3] in sept:
+                    sept[row[3]] += float(row[4])
+                else:
+                    sept[row[3]] = float(row[4])
+
+            print(row[0][0])
+
             line_count += 1
     print(f'Processed {line_count} lines.')
 
+    del mydict['PAYMENT']
+
     for k, v in mydict.items():
         mydict[k] = round(v,2)
+        x += v
 
+    for k, v in sept.items():
+        sept[k] = round(v,2)
+        x += v
+    mydict['TOTAL'] = round(sum(mydict.values()),2)
     print(mydict)
+    print(sept)
